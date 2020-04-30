77d950efb5f4c9168aa87925e10fd972df95a78a
Krzysztof Kowaczek <krzysiu99234@gmail.com>
2020-04-30 14:12:01 +0200

    Done modification as asked

--- z4/src/main.cpp
+++ z4/src/main.cpp
@@ -13,10 +13,23 @@ int main(int argc, char *argv[])
   ifstream inFile;
   SystemOfLinearEquations<Complex, 5> complexSystem;
   SystemOfLinearEquations<double, 5> doubleSystem;
+  Complex complexNumber = {3, -5};
+  Matrix<double, 2> complexMatrix;
+  complexMatrix[0][0] = complexMatrix[1][1] = complexNumber.re;
+  complexMatrix[0][1] = complexNumber.im;
+  complexMatrix[1][0] = -complexNumber.im;
+
+  cout << endl;
+  cout << "         Modul do kwadratu liczby zespolonej: "
+       << AbsSquared(complexNumber) << endl;
+  cout << "Wyznacznik reprezentacji l. zesp. w macierzy: " 
+       << complexMatrix.determinant() << endl << endl;
+
   if(argc < 2)
   {
     cout << "Nie podano argumentow wywolania programu!" << endl;
-    cout << "Poprawne wywolanie programu brzmi: " << endl << argv[0] << " r|z" << endl;
+    cout << "Poprawne wywolanie programu brzmi: " << endl << argv[0] 
+         << " r|z" << endl;
   }
   else
   {
